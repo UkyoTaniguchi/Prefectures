2d9d0bb85f6707ff68b3e792ca5d91a8a858b09a
Ukyo <ukyou20011121@gmail.com>
2024-12-12 11:34:17 +0900

    update: コンポーネント表示

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Title from "./components/Title";
+import Checkbox from "./components/Checkbox";
+import Graph from "./components/Graph";
 
 export default function Home() {
   return (
     <div>
       <Title />
+      <Checkbox />
+      <Graph />
     </div>
   );
 }
