a4efd07b3f9cccaf6e5f7cfa7ca41be10ded4801
Ukyo <ukyou20011121@gmail.com>
2024-12-17 13:43:42 +0900

    add: 都道府県表示チェックボックスコンポーネント表示

--- app/components/Checkbox.tsx
+++ app/components/Checkbox.tsx
@@ -1,5 +1,40 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+
+type Prefecture = {
+  prefCode: number;
+  prefName: string;
+};
 
 export default function Checkbox() {
-  return <div>checkbox</div>;
+  const [prefectures, setPrefectures] = useState<Prefecture[]>([]);
+  useEffect(() => {
+    const fetchPrefectures = async () => {
+      try {
+        const response = await fetch("/api/getPrefectures");
+        const data = await response.json();
+        setPrefectures(data.result);
+      } catch (error) {
+        console.error("データ取得に失敗しました", error);
+      }
+    };
+
+    fetchPrefectures();
+  }, []);
+
+  return (
+    <div>
+      <h1>都道府県</h1>
+      <ul>
+        {prefectures.map((pref) => (
+          <li key={pref.prefCode}>
+            <label>
+              <input type="checkbox" value={pref.prefName} />
+              {pref.prefName}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
