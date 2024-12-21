4b83155739df9bae62fcb62f9c0426033b0667aa
Ukyo <ukyou20011121@gmail.com>
2024-12-21 22:17:02 +0900

    update: グラフの動的な変更

--- app/components/Graph.tsx
+++ app/components/Graph.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
@@ -30,28 +30,108 @@ export default function Graph({
   selectedPrefectures,
   prefectures,
 }: GraphProps) {
+  const [population, setPopulation] = useState<
+    { year: number; value: number; rate: number }[]
+  >([]);
+  const [selectedLabel, setSelectedLabel] = useState<string>("総人口");
+
+  const labelcheck = (value: string) => {
+    setSelectedLabel(value);
+  };
+
+  useEffect(() => {
+    const totalPopulationData = Object.values(selectedPrefectures)
+      .filter((data) => data !== null)
+      .flatMap((population) =>
+        population!.data
+          .filter((category) => category.label === selectedLabel)
+          .flatMap((category) => category.data)
+      );
+    setPopulation(totalPopulationData);
+  }, [selectedPrefectures, selectedLabel]);
+
   const options: Highcharts.Options = {
     title: {
-      text: "My chart",
+      text: "総人口",
     },
-    series: [
-      {
-        type: "line",
-        data: [1, 2, 3],
+    xAxis: {
+      title: {
+        text: "年",
       },
-    ],
+      categories: population.map((item) => item.year.toString()),
+    },
+    yAxis: {
+      title: {
+        text: "人口数",
+      },
+    },
+    series:
+      population.length > 0
+        ? [
+            {
+              type: "line",
+              name: "総人口",
+              data: population.map((item) => item.value),
+            },
+          ]
+        : [],
   };
 
   const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
-
   return (
     <div>
+      <div>
+        <div>
+          <input
+            type="radio"
+            name="population"
+            value="総人口"
+            onChange={(e) => labelcheck(e.target.value)}
+            checked={selectedLabel === "総人口"}
+          />
+          総人口
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="population"
+            value="年少人口"
+            onChange={(e) => labelcheck(e.target.value)}
+            checked={selectedLabel === "年少人口"}
+          />
+          年少人口
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="population"
+            value="生産年齢人口"
+            onChange={(e) => labelcheck(e.target.value)}
+            checked={selectedLabel === "生産年齢人口"}
+          />
+          生産年齢人口
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="population"
+            value="老年人口"
+            onChange={(e) => labelcheck(e.target.value)}
+            checked={selectedLabel === "老年人口"}
+          />
+          老年人口
+        </div>
+      </div>
       <div>グラフ</div>
-      <HighchartsReact
-        highcharts={Highcharts}
-        options={options}
-        ref={chartComponentRef}
-      />
+      {population.length > 0 ? (
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={options}
+          ref={chartComponentRef}
+        />
+      ) : (
+        <p>データがありません</p>
+      )}
     </div>
   );
 }
